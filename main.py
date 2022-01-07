c7565ea3a8c3d901bef1b6bf24d5490f3180bd97
Christian F <96458958+cfeliciano7792@users.noreply.github.com>
2022-01-07 22:34:21 +0000

    removed letter a from line 7 and 13

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ year = int(input("Which year do you want to check? "))
 
 #Write your code below this line 👇
 if year % 4 > 0:
-  print("Not a leap year.")
+  print("Not leap year.")
 elif year % 100 > 0:
   print("Leap year.")
 elif year % 400 == 0:
   print("Leap year.")
 else:
-  print("Not a leap year.")
+  print("Not leap year.")
