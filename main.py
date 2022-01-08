4a97ac2d6d8c5d6d96a75746441826dcf89e39fa
Christian F <96458958+cfeliciano7792@users.noreply.github.com>
2022-01-08 01:35:15 +0000

    Fixed type on line 7 and 13

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ year = int(input("Which year do you want to check? "))
 
 #Write your code below this line 👇
 if year % 4 > 0:
-  print("Not leap year.")
+  print("Not a leap year.")
 elif year % 100 > 0:
   print("Leap year.")
 elif year % 400 == 0:
   print("Leap year.")
 else:
-  print("Not leap year.")
+  print("Not a leap year.")
